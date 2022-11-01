22be85b792c83ad9f22579d090a5f57a7282cd52
Ducnm <ducnm2k@gmail.com>
2022-11-01 15:43:26 +0700

    create new model/product & fetch api product feature in index

---
+++ js/controller/index.js
@@ -0,0 +1,47 @@
+
+
+function fetchDataFromAPI() {
+    let promise = axios({
+        method: "GET",
+        url: `https://shop.cyberlearn.vn/api/Product`
+    });
+
+    promise.then((res) => {
+        // console.log(res.data.content);
+        renderProductFeature(res.data.content);
+    });
+
+    promise.catch((err) => {
+        console.log(err)
+    });
+}
+
+function renderProductFeature(arr) {
+    let content = ``;
+    for (let i = 0; i < arr.length; i++) {
+        var product = arr[i];
+        content += `
+            <div class="card-item col-4">
+                <div class="card-body">
+                    <a href="#">
+                        <img src="${product.image}" alt="" />
+                    </a>
+                    <h3 id="card-name">${product.name}</h3>
+                    <p id="card-description">${product.shortDescription}</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#">
+                        <p id="text">Buy now</p>
+                    </a>
+                    <span id="outer">
+                        <p id="inner"> ${product.price}$ </p>
+                    </span>
+                </div>
+            </div>
+            `;
+    }
+
+    document.querySelector('#product-feature').innerHTML = content;
+}
+
+fetchDataFromAPI();
